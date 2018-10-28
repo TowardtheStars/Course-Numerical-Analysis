4ad4bafe4aa99620418f1c0539b949fb264b0298
TowardtheStars <hty16@mail.ustc.edu.cn>
2018-10-28 10:54:59 +0800

    增加注释

--- src/homework_03.cpp
+++ src/homework_03.cpp
@@ -8,38 +8,33 @@
 
 using namespace std;
 
-void displayMat(Mat& result)
-{
-    for (int i = 0; i < result.getRow(); ++i)
-    {
-        for (int j = 0; j < result.getCol(); ++j)
-        {
-            cout << result[i][j]<<',';
-        }
-        cout<<endl;
-    }
-}
-
 int main()
 {
+    // 原始数据
     vector<double>
             data_x = {0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00, 2.25, 2.50},
             data_y = {1.284,1.648,2.117,2.718,3.427,2.798,3.534,4.456,6.465,5.894};
 
+    // 声明矩阵
     Mat A(data_x.size(), 2), ATA, ATY, Y(data_x.size(), 1), result;
+    // 计算系数矩阵
     for (int i = 0; i < A.getRow(); ++i)
     {
         A[i][0] = sin(data_x[i]);
         A[i][1] = cos(data_x[i]);
         Y[i][0] = data_y[i];
     }
+    // 计算A^T A 和 A^T Y
     ATA = Mat::dot(Mat::transpose(A), A);
     ATY = Mat::dot(Mat::transpose(A), Y);
+    // 解方程组得到结果
     result = solveLinear(ATA, ATY);
 
+    // 输出结果
     double a = result[0][0], b = result[1][0], sum = 0.0, temp;
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
+    // 计算均方误差
     for (int i = 0; i < data_x.size(); ++i)
     {
         temp = a * sin(data_x[i]) + b * cos(data_x[i]) - data_y[i];
