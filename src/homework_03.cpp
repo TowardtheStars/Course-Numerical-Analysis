4767639aaad81a0bc38af1515abc2f8893ab5886
TowardtheStars <hty16@mail.ustc.edu.cn>
2018-10-28 10:43:50 +0800

    第三次作业完成

--- src/homework_03.cpp
+++ src/homework_03.cpp
@@ -7,13 +7,26 @@
 #include "../lib/algebra/matrix.h"
 
 using namespace std;
+
+void displayMat(Mat& result)
+{
+    for (int i = 0; i < result.getRow(); ++i)
+    {
+        for (int j = 0; j < result.getCol(); ++j)
+        {
+            cout << result[i][j]<<',';
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
     vector<double>
             data_x = {0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.00, 2.25, 2.50},
             data_y = {1.284,1.648,2.117,2.718,3.427,2.798,3.534,4.456,6.465,5.894};
 
-    Mat A(data_x.size(), 2), ATA, ATY, Y(data_x.size(), 1);
+    Mat A(data_x.size(), 2), ATA, ATY, Y(data_x.size(), 1), result;
     for (int i = 0; i < A.getRow(); ++i)
     {
         A[i][0] = sin(data_x[i]);
@@ -22,15 +35,18 @@ int main()
     }
     ATA = Mat::dot(Mat::transpose(A), A);
     ATY = Mat::dot(Mat::transpose(A), Y);
-//    for (int i = 0; i < ATY.getRow(); ++i)
-//    {
-//        for (int j = 0; j < ATY.getCol(); ++j)
-//        {
-//            cout << ATY[i][j]<<',';
-//        }
-//        cout<<endl;
-//    }
-    cout << ATY.getRow() << endl;
+    result = solveLinear(ATA, ATY);
 
+    double a = result[0][0], b = result[1][0], sum = 0.0, temp;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    for (int i = 0; i < data_x.size(); ++i)
+    {
+        temp = a * sin(data_x[i]) + b * cos(data_x[i]) - data_y[i];
+        temp *= temp;
+        sum += temp;
+    }
+    sum /= data_x.size();
+    cout << "均方误差 = " << sum << endl;
     return 0;
 }
